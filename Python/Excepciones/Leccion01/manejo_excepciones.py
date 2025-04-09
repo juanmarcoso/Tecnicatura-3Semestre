5d102b00c78cb3c37b607d9c2bbfa4fe6e927b38
Juan Marcos Orellana <juanmarcoso@gmail.com>
2025-04-09 00:21:55 -0300

    Handling errors2

--- Python/Excepciones/Leccion01/manejo_excepciones.py
+++ Python/Excepciones/Leccion01/manejo_excepciones.py
@@ -1,11 +1,15 @@
 resultado = None
-a = '10'
+a = 10
 b = 0
 
 try:
-    resultado = a / b # Modificamos 
-except ZeroDivisionError as e: # Nos va a dar un error de type por lo que es un string
-    print(f'Ocurrio un error: {e}')
+    resultado = a / b # Modificamos
+except ZeroDivisionError as e:
+    print(f'ZeroDivisionError - Ocurrio un error: {type(e)}')
+except TypeError as e:
+    print(f'TypeError - Ocurrio un error: {type(e)}')
+except Exception as e: # La clase padre puede manejar todo tipo error
+    print(f'Exception - Ocurrio un error: {e}')
 
 print(f'El resultado es: {resultado}')
 print('seguimos...')
