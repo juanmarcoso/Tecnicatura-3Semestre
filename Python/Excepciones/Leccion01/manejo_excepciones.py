7f464700efd5ec579b083a7dfc11ec35dd1db53f
Juan Marcos Orellana <juanmarcoso@gmail.com>
2025-04-09 00:13:41 -0300

    Handling errors

--- Python/Excepciones/Leccion01/manejo_excepciones.py
+++ Python/Excepciones/Leccion01/manejo_excepciones.py
@@ -1,4 +1,11 @@
+resultado = None
+a = '10'
+b = 0
+
 try:
-    10/0
-except Exception as e:
+    resultado = a / b # Modificamos 
+except ZeroDivisionError as e: # Nos va a dar un error de type por lo que es un string
     print(f'Ocurrio un error: {e}')
+
+print(f'El resultado es: {resultado}')
+print('seguimos...')
