e1a66ad8f4c4edb47ac6f5f5f2f97991de96c529
Juan Marcos Orellana <juanmarcoso@gmail.com>
2025-04-10 08:24:48 -0300

    Starting with the class of Java

---
+++ Java/Leccion01/MundoPC/src/ar/com/system2025/mundopc/Raton.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ar.com.system2025.mundopc;
+
+/**
+ *
+ * @author juan
+ */
+public class Raton {
+    
+}
