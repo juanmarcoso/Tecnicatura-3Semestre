69ab3fc54f6b9174fccde4383288a50ab2737ed9
Juan Marcos Orellana <juanmarcoso@gmail.com>
2025-04-10 10:07:53 -0300

    Making Computadora class

---
+++ Java/Leccion01/MundoPC/src/ar/com/system2025/mundopc/Computadora.java
@@ -0,0 +1,77 @@
+package ar.com.system2025.mundopc;
+
+public class Computadora {
+    
+    private final int idComputadora;
+    private String nombre;
+    private Monitor monitor;
+    private Teclado teclado;
+    private Raton raton;
+    private static int contadorComputadora;
+       
+// Constructor vacio
+private Computadora(){
+    this.idComputadora = ++Computadora.contadorComputadora;
+}
+
+// Constructor2
+public Computadora(String nombre, Monitor monitor, Teclado teclado, Raton raton){
+    
+    this();
+    this.nombre = nombre;
+    this.monitor = monitor;
+    this.teclado = teclado;
+    this.raton = raton;
+    
+}
+
+    public int getIdComputadora(){
+        return idComputadora;
+    }
+    
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Monitor getMonitor() {
+        return this.monitor;
+    }
+
+    public void setMonitor(Monitor monitor) {
+        this.monitor = monitor;
+    }
+
+    public Teclado getTeclado() {
+        return this.teclado;
+    }
+
+    public void setTeclado(Teclado teclado) {
+        this.teclado = teclado;
+    }
+
+    public Raton getRaton() {
+        return this.raton;
+    }
+
+    public void setRaton(Raton raton) {
+        this.raton = raton;
+    }
+
+    @Override
+    public String toString() {
+        return "Computadora{" + 
+                "IdComputadora= " + idComputadora + 
+                "Nombre= " + nombre + 
+                "Monitor= " + monitor + 
+                "Teclado= " + teclado + 
+                "Raton= " + raton + '}';
+    }
+    
+    
+
+}
+
