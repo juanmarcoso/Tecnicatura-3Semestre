7a965922432a2c27a59e6f39796037caaa6b885e
Juan Marcos Orellana <juanmarcoso@gmail.com>
2025-04-10 08:33:22 -0300

    Making child class of mouse

--- Java/Leccion01/MundoPC/src/ar/com/system2025/mundopc/Raton.java
+++ Java/Leccion01/MundoPC/src/ar/com/system2025/mundopc/Raton.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ar.com.system2025.mundopc;
 
-/**
- *
- * @author juan
- */
-public class Raton {
+public class Raton extends DispositivoEntrada {
     
+    private final int idRaton;
+    private static int contadorRaton;
+    
+    public Raton(String tipoEntrada, String marca){
+        
+        super(tipoEntrada, marca);
+        this.idRaton = ++Raton.contadorRaton;
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Raton{" + "idRaton= " + idRaton + ", " + super.toString() + '}';
+    }
+      
 }
