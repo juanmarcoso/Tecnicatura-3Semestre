03a9cd0bf95352eb45dab00e77b9c9d0aed5d6ec
Juan Marcos Orellana <juanmarcoso@gmail.com>
2025-04-12 00:27:22 -0300

    JavaScript proyect MundoPC adding class

--- JavaScript/Leccion01/MundoPC.js
+++ JavaScript/Leccion01/MundoPC.js
@@ -61,6 +61,31 @@ class Teclado extends DispositivoEntrada{
     }
 }
 
-let teclado1 = new Teclado('Cable', 'HP');
+let teclado1 = new Teclado('USB', 'HP');
 console.log(teclado1.toString())
+let teclado2 = new Teclado('Bluetooth', 'MSI')
+console.log(teclado2.toString())
 
+class Monitor{
+    static contadorMonitores = 0;
+
+    constructor(marca, tamanio){
+        this._idMonitor = ++Monitor.contadorMonitores;
+        this._marca = marca;
+        this._tamanio = tamanio;
+    }
+
+    get idMonitor(){
+        return this._idMonitor;
+    }
+
+    toString(){
+        return `Monitor: [idMonitor: ${this._idMonitor}, marca: ${this._marca}, tamaño: ${this._tamanio}]`;
+    }
+
+}
+
+let monitor1 = new Monitor('HP', 15);
+let monitor2 = new Monitor('Dell', 27)
+console.log(monitor1.toString())
+console.log(monitor2.toString())
