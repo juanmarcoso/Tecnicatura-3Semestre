5f63c0dc36fbc4a64c2759743882725f341d7da9
JuanMaDev <juanmarcoso@gmail.com>
2025-04-14 23:38:46 -0300

    Agregando informacion
    Agregamos informacion al archivo creado por el script

--- Python/Archivos/Leccion02/manejo_archivos.py
+++ Python/Archivos/Leccion02/manejo_archivos.py
@@ -1,6 +1,8 @@
 # Declaramos una variable 
 try:
     archivo = open('prueba.txt', 'w') # La w es para escritura
+    archivo.write('Programamos con diferentes tipos de archivos, ahora en txt\n')
+    archivo.write('Con esto terminamos.')
 except Exception as e:
     print(e)
 finally: # Siempre se ejecuta
