6c8cd5e65bf6aace30b258bd0ea80cbc2da0a850
JuanMaDev <juanmarcoso@gmail.com>
2025-04-15 17:41:12 -0300

    usando with

---
+++ Python/Archivos/Leccion02/archivos_con_with.py
@@ -0,0 +1,7 @@
+# Manejo de contexto con with: sintaxis simplificada, abre y cierra el archivo
+with open('prueba.txt', 'r', encoding='UTF-8') as archivo:
+    print(archivo.read())
+
+# En el contexto de with lo que se ejecuta de manera automatica
+# Utiliza diferentes métodos: __enter__ este es el que abre
+# El metodo que cierra: __exit__
