0bef20fccb7aa8320bee4e743310404c6e51f41e
JuanMaDev <juanmarcoso@gmail.com>
2025-04-14 23:48:29 -0300

    Especificamos el tipo de caracter utilizado

--- Python/Archivos/Leccion02/manejo_archivos.py
+++ Python/Archivos/Leccion02/manejo_archivos.py
@@ -1,10 +1,13 @@
 # Declaramos una variable 
 try:
-    archivo = open('prueba.txt', 'w') # La w es para escritura
-    archivo.write('Programamos con diferentes tipos de archivos, ahora en txt\n')
+    archivo = open('prueba.txt', 'w', encoding='UTF-8') # La w es para escritura
+    archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n')
+    archivo.write('Los acentos son importantes para las palabras\n')
+    archivo.write('como por ejemplo: acción, ejecución y producción.\n')
     archivo.write('Con esto terminamos.')
 except Exception as e:
     print(e)
 finally: # Siempre se ejecuta
     archivo.close() # Con esto siempre se cierra el archivo
     
+#archivo.write("Todo quedó perfecto") No se puede ejecutar porque ya esta cerrado el archivo
