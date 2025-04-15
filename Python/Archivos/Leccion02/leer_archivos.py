0760f48af50495ecb6522a7d627493a98873223a
JuanMaDev <juanmarcoso@gmail.com>
2025-04-15 00:01:23 -0300

    Sumando mas teoria

--- Python/Archivos/Leccion02/leer_archivos.py
+++ Python/Archivos/Leccion02/leer_archivos.py
@@ -1,3 +1,15 @@
+"""
+Las letras para el manejo de archivos en python son: 
+'r': read
+'a': append
+'w': write
+'x': crea un archivo
+'t': para tipo de archivo text
+'b': para archivos binarios
+'w+': Para escribir y leer informacion
+'r+': Para leer y escribir infomrción
+
+"""
 archivo = open('prueba.txt', 'r', encoding='UTF-8')
 #print(archivo.read())
 
