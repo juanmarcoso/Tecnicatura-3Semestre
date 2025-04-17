ab62a1ce9d790ffcab25976ed0f9434958bb6ac0
JuanMaDev <juanmarcoso@gmail.com>
2025-04-17 07:29:36 -0300

    JavaScript - Agregando quiz

---
+++ JavaScript/Quiz/quiz.js
@@ -0,0 +1,6 @@
+let hora = 23;
+if (hora <= 23) {
+          console.log("Se entrega el trabajo en tiempo y forma");
+} else {
+          console.log("No recibo el trabajo, fuera del tiempo pactado");
+}
