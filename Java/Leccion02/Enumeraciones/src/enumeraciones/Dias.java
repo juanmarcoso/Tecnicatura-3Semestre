f25eb494c984035a05c7163cd1b3fa4b118c21ee
JuanMaDev <juanmarcoso@gmail.com>
2025-04-17 00:54:35 -0300

    JAVA- Trabajando con num

---
+++ Java/Leccion02/Enumeraciones/src/enumeraciones/Dias.java
@@ -0,0 +1,14 @@
+package enumeraciones;
+
+public enum Dias {
+    
+    // Armamos unal ista de valores que tiene atributos publicos, estaticos y constantes
+    LUNES, 
+    MARTES, 
+    MIERCOLES, 
+    JUEVES, 
+    VIERNES, 
+    SABADO, 
+    DOMINGO
+    
+}
---
+++ Java/Leccion02/Enumeraciones/src/test/TestEnumeraciones.java
@@ -0,0 +1,44 @@
+package test;
+
+import enumeraciones.Dias;
+
+public class TestEnumeraciones {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Dia 1: " + Dias.LUNES);
+        indicarDiaSemana(Dias.LUNES); // Las enumeraciones se tratan como cadenas
+        // No deben utilizar comillas, se accede a traves del operador éde punto
+        
+    }
+    
+    private static void indicarDiaSemana(Dias dias){
+        
+        switch(dias){
+            case LUNES:
+                System.out.println("Primer dia de la semana");
+                break;
+            case MARTES:
+                System.out.println("Segundo dia de la semana");
+                break;
+            case MIERCOLES:
+                System.out.println("Tercer dia de la semana");
+                break;
+           case JUEVES:
+                System.out.println("Cuarto dia de la semana");
+                break;
+           case VIERNES:
+               System.out.println("Quinto dia de la semana");
+               break;
+           case  SABADO:
+               System.out.println("Sexto dia de la semana");
+           case DOMINGO:
+               System.out.println("Septimo dia de la semana");
+               break;
+           default:
+               System.out.println("Dia no valido. Debe ser algun día de la semana.");
+        }
+        
+    }
+    
+}
