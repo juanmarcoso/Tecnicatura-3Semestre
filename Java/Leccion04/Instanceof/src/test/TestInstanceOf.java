13f57d74abdcb63aad071f65311c9699b211d7e8
JuanMaDev <juanmarcoso@gmail.com>
2025-05-04 00:36:54 -0300

    Java - Agregando instanceof

---
+++ Java/Leccion04/Instanceof/src/test/TestInstanceOf.java
@@ -0,0 +1,25 @@
+package test;
+
+import domain.*;
+
+
+public class TestInstanceOf {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("Juan", 2000);
+        determinarTipo(empleado1);
+        empleado1 = new Gerente("Jose", 5000, "Sistemas");
+        
+    }
+
+    public static void determinarTipo(Empleado empleado){
+        if(empleado instanceof Gerente){
+            System.out.println("Es de tipo Gerente");
+        }
+        else if(empleado instanceof Empleado){
+            System.out.println("Es de tipo empleado");
+        }
+        else if(empleado instanceof Object){
+            System.out.println("Es de tipo object");
+        }
+    }
+}
