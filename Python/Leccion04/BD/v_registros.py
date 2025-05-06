bb46102160658c5a875aa0c3218e14c50aaa0e66
JuanMaDev <juanmarcoso@gmail.com>
2025-05-06 18:07:58 -0300

    Python- Agregando lectura dee registro con for

--- Python/Leccion04/BD/v_registros.py
+++ Python/Leccion04/BD/v_registros.py
@@ -21,19 +21,20 @@ try:
         with conexion.cursor() as cursor:
             # Consulta SQL para seleccionar todos los registros de la tabla persona
             # Al usar %s, psycopg2 escapa automáticamente los valores para prevenir inyección SQL.
-            sentencia = 'SELECT * FROM persona WHERE id_persona = %s' #Placeholder
+            sentencia = 'SELECT * FROM persona WHERE id_persona IN (1, 2)' 
             
             # Creamos una variable
-            id_persona = input("Digite el numero Id a buscar: ")
+            #id_persona = input("Digite el numero Id a buscar: ")
             
             # Ejecuta la consulta SQL
-            cursor.execute(sentencia, (id_persona, ))
+            cursor.execute(sentencia)
             
             # Obtiene todos los registros resultantes de la consulta
-            registros = cursor.fetchone()
+            registros = cursor.fetchall()
             
-            # Imprime los registros en la consola
-            print(registros)
+            # Imprime los registros en la consola con un for
+            for registro in registros:    
+                print(registro)
 
 except Exception as e:
     # Manejo de errores: captura cualquier excepción durante la ejecución
@@ -43,14 +44,3 @@ finally:
     # Este bloque se ejecuta siempre, asegurando que la conexión se cierre
     conexion.close()
     print("Conexión cerrada correctamente")
-
-#print(conexion)
-# cursor = conexion.cursor()
-# sentencia = 'SELECT * FROM persona'
-# cursor.execute(sentencia) # De esta manera ejecutamos la sentencia
-# # Recuperamos todos los registros como tuplas dentro de una tupla
-# registros = cursor.fetchall() #
-# print(registros) #[(1, 'Juan', 'Perez', 'jperez@mail.com'), (2, 'Carla', 'Gomez', 'kgomez@gmail.com')]
-
-#cursor.close() # Cerramos la conexion con el objeto cursor
- # Cerramos la conexion con nuestra base de datos
