e3675d9789d654e3cf086e9c9e8ca6b3a3bcc4b4
JuanMaDev <juanmarcoso@gmail.com>
2025-05-13 00:09:27 -0300

    Python - Ahora trabajando con with

--- Python/Leccion04/BD/transacciones_3.py
+++ Python/Leccion04/BD/transacciones_3.py
@@ -22,27 +22,25 @@ conexion = bd.connect(
 # En este modo debemos gestionar manualmente los commits y rollbacks
 # Nota: La forma preferida es usar un contexto 'with' que maneja esto automáticamente
 try:
-    conexion.autocommit = False  # Desactivamos autocommit para controlar la transacción
-    cursor = conexion.cursor()
-    
-    # Query de inserción con parámetros
-    sentencia = 'INSERT INTO persona (nombre, apellido, email) VALUES (%s, %s, %s)'
-    valores = ('Jorge', 'Prol', 'jprol@mail.com')
-    cursor.execute(sentencia, valores)
+    with conexion:
+        with conexion.cursor() as cursor:
+            
+            # Query de inserción con parámetros
+            sentencia = 'INSERT INTO persona (nombre, apellido, email) VALUES (%s, %s, %s)'
+            valores = ('Alex', 'Rojas', 'arojas@mail.com')
+            cursor.execute(sentencia, valores)
 
-    # Query de actualizacion con parametros
-    sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
-    valores = ('Juan Carlos', 'Perez', 'jcperez@mail.com', 1)
-    cursor.execute(sentencia, valores)
-
-    conexion.commit()  # Confirmamos la transacción manualmente
-    print('Transacción completada con éxito')
-
+            # Query de actualizacion con parametros
+            sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+            valores = ('Juan Carlos', 'Rolda', 'jcroldan@mail.com', 1)
+            cursor.execute(sentencia, valores)
+            
 except Exception as e:
-    conexion.rollback()  # Revertimos la transacción en caso de error
     print(f'Error en la transacción - rollback ejecutado: {e}')
 
 finally:
     # Cerramos la conexión siempre, independientemente del resultado de la transacción
     conexion.close()
     print("Conexión cerrada correctamente")
+
+print('Transacción completada con éxito')
