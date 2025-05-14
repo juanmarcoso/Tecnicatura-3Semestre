4c6f68aa121b7b549df996d89aac46f465bbf4e6
JuanMaDev <juanmarcoso@gmail.com>
2025-05-14 01:35:04 -0300

    JavaScript - Agregando clase06 Func Callback

---
+++ JavaScript/Leccion06/FuncionesCallBack.js
@@ -0,0 +1,38 @@
+// Llamando a la funcion antes de ser declaradas es un ejemplo de hosting
+miFuncion1()
+miFuncion2()
+
+/*
+ * Declaración de miFuncion1
+ * Imprime 'Funcion 1' en la consola
+ */
+function miFuncion1(){
+    console.log('Funcion 1');
+}
+
+/*
+ * Declaración de miFuncion2
+ * Imprime 'Funcion 2' en la consola
+ */
+function miFuncion2(){
+    console.log('Funcion 2')
+}
+
+//Funcion de tipo callback asignada a una variable
+let impr = function imprimir(mensaje){
+    console.log( mensaje )
+}
+
+/*
+ * Función que suma dos operandos y usa un callback para mostrar el resultado
+ * @param {number} op1 - Primer operando
+ * @param {number} op2 - Segundo operando
+ * @param {function} funcionCallBack - Función callback que recibe el resultado
+ */
+function sumar (op1, op2, funcionCallBack){
+    let res = op1 + op2
+    funcionCallBack(`Resultado: ${res}`)
+}
+
+// Llamada a la función sumar pasando 5, 3 y la función impr como callback
+sumar(5, 3, impr);
