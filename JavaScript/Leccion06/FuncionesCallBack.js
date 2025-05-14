567a60d7d87b559a2c4887982a89ed06a6b42ffa
JuanMaDev <juanmarcoso@gmail.com>
2025-05-14 01:45:19 -0300

    JavaScript- Agregando setTimeout

--- JavaScript/Leccion06/FuncionesCallBack.js
+++ JavaScript/Leccion06/FuncionesCallBack.js
@@ -36,3 +36,14 @@ function sumar (op1, op2, funcionCallBack){
 
 // Llamada a la función sumar pasando 5, 3 y la función impr como callback
 sumar(5, 3, impr);
+
+//Llamadas asincronas con uso de setTimeout
+function miFuncionCallback(){
+    console.log('Saludo asincrono despues de 3 segundos')
+}
+
+setTimeout(miFuncionCallback, 3000); //En milisegundos decimos que queremos que aparezca en 3 segundos
+
+setTimeout(function(){ console.log("Saludo asincrono 2 despues de 5 segundos")}, 5000);
+
+setTimeout( () => console.log('Saludo asincrono 3 despues de 9 segundos'), 9000);
